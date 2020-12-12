455d1a71e3299afd72dc453fa92a036cf5ef803a
B3ns44d <abdsamad23bnsaad@gmail.com>
2020-12-12 17:16:13 +0000

    format the bubblesort.js file

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,10 +1,13 @@
 function bubbleSort(inputArray) {
   var num = inputArray.length;
+
   for (var i = num - 1; i >= 0; i--) {
     for (var j = 1; j <= i; j++) {
       if (inputArray[j - 1] > inputArray[j]) {
         var temp = inputArray[j - 1];
+
         inputArray[j - 1] = inputArray[j];
+
         inputArray[j] = temp;
       }
     }
